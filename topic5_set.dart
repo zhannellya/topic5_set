7395ec9b3dac794085f5b5385fb2d73d77e9e1c3
Rysbai Zhanel <120437053+zhannellya@users.noreply.github.com>
2023-11-04 11:18:01 +0300

    Add files via upload

---
+++ topic5_set.dart
@@ -0,0 +1,59 @@
+void main() {
+  //task1
+  // Set<int> first = {1, 2, 3, 4};
+  // Set<int> second = {1, 2};
+
+  // var set = first.intersection(second);
+
+  // if (set == null) {
+  //   print("False");
+  // } else {
+  //   print("True");
+  // }
+
+  //task2
+  // var set = {"Tom", "Alice", "Bob", "Sam", "Kate"};
+  // set.removeAll({"Alice", "Bob", "Tom", "Sam", "Kate"});
+  // print(set);
+
+  //task3
+  //task4
+  // Set<int> set = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  // print(set.length);
+
+  //task5
+  // Set<int> set = {1, 2, 3};
+  // int number = 1;
+
+  // print(set.contains(number));
+
+  //task6
+  // Set<int> setA = {1, 2, 3, 4, 5};
+  // Set<int> setB = {3, 6, 7, 5};
+
+  // Set<int> intersection = setA.intersection(setB);
+  // print(intersection);
+
+  //task7
+  // Set<int> setA = {1, 6, 7, 4, 5};
+  // Set<int> setB = {8, 6, 7, 5};
+
+  // Set<int> difference = setA.difference(setB);
+  // print(difference);
+
+  // Set<int> differenceB = setB.difference(setA);
+  // print(differenceB);
+
+  //task8
+  // var set1 = {1, 3, 5, 7, 9};
+  // var set2 = {2, 4, 5, 6, 9};
+  // Set<int> intersection = set1.intersection(set2);
+  // set1.removeAll(intersection);
+  // print(set1);
+
+  //task13
+  // Set<int> set1 = {8, 7, 3, 4, 5, 6, 1};
+  // Set<int> set2 = {9, 8, 7, 6, 5};
+  // print(set1.difference(set2));
+  // print(set2.difference(set1));
+}
